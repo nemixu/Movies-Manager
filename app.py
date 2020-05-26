b9a38cdc34a04cd96778b95d37a9acc24a757f0a
nemixu <s.r.seagrave@gmail.com>
2020-05-26 21:44:26 +0100

    added database connection with open vars, will be hidden in deployed project

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if path.exists("env.py"):
 
 # Connection to Database
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_DBNAME"] = "movies_database"
 app.config["MONGO_URI"] = "mongodb+srv://root:winter22@myfirstcluster-0xnxg.mongodb.net/movies_database?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
