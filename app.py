3d4f08436c56fa8b538367384e988afa3dfef6e4
nemixu <s.r.seagrave@gmail.com>
2020-05-26 21:21:39 +0100

    added some imports also added connections to the database which are local, will be hidden in env vars for prod

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import os 
-from flask import Flask, render_template, redirect, request, url_for
+from os import path
+import time
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
+if path.exists("env.py"):
+    import env
 
+# Connection to Database
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = "mongodb+srv://root:winter22@myfirstcluster-0xnxg.mongodb.net/movies_database?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
