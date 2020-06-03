d067efb66d961d870c9314130ef09bb4e164881f
nemixu <s.r.seagrave@gmail.com>
2020-06-03 22:22:20 +0100

    updated search root, for loop added seperating data and returning data with if statement

---
+++ app.py
@@ -0,0 +1,66 @@
+import os 
+from os import path
+import time
+import requests
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
+if path.exists("env.py"):
+    import env
+
+# Connection to Database
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "movies_database"
+app.config["MONGO_URI"] = "mongodb+srv://root:winter22@myfirstcluster-0xnxg.mongodb.net/movies_database?retryWrites=true&w=majority"
+
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        searchTerm = request.form['search-term']
+        apikey = "3c0dea9f"
+        api = requests.get("http://www.omdbapi.com/?apikey={}&s={}".format(apikey, searchTerm))
+        data = api.json()
+        print(data)
+        returnResults = list()
+        for movies in data['Search']:
+            title = movies['Title']
+            year = movies['Year']
+            poster = movies['Poster']
+            movieDetails = [title, year, poster]
+            returnResults.append(movieDetails)
+        print(returnResults)
+        return render_template('search.html', returnResults=returnResults)
+    else:
+        return render_template('search.html')
+            
+            
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template('login.html') 
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html') 
+ 
+ 
+ 
+  
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port= os.environ.get('PORT'),
+            debug=True)
+
+
